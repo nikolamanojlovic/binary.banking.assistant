0568099e9c7c3faece3c318fa65b07e7fb1dad69
Nikola Manojlović <manojlovic.nikola95@gmail.com>
2018-11-17 15:55:09 +0100

    Racun i konstante

---
+++ Domen/Konstante.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domen.Konstante
+{
+    public static class Konstante
+    {
+        public static class Opste
+        {
+            public static readonly string ZAREZ = ",";
+        }
+
+        public static class SQL
+        {
+            public static readonly string FORMAT_DATUMA = "yyyy-MM-dd HH:mm:ss.fff";
+        }
+
+        public static class TabelaRacun
+        {
+            public static readonly string NAZIV_TABELE = "account";
+            public static readonly string PK_RACUN_ID = "account_id";
+            public static readonly string TABELA_RACUN_UBACI = " '{0}', '{1}', '{2}', '{3}' ";
+        }
+
+        public static class TabelaKlijent
+        {
+            public static readonly string NAZIV_TABELE = "client";
+            public static readonly string PK_KLIJENT_ID = "client_id";
+            public static readonly string TABELA_KLIJENT_UBACI = " '{0}', '{1}', '{2}', '{3}', '{4}', '{5}', '{6}', '{7}', '{8}' ";
+        }
+
+        public static class TabelaTipKredita
+        {
+            public static readonly string NAZIV_TABELE = "loan_type";
+            public static readonly string PK_TIP_RACUNA_ID = "loan_type_id";
+            public static readonly string TABELA_TIP_KREDITA_UBACI
+                = " '{0}', '{1}', '{2}', '{3}', '{4}' ";
+        }
+    }
+}
---
+++ Domen/Racun.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domen.Konstante;
+
+namespace Domen
+{
+    public class Racun
+    {
+        private long id;
+        private String brojRacuna;
+        private TipRacuna tip;
+        private DateTime datumKreiranja;
+
+        public DateTime DatumKreiranja
+        {
+            get { return datumKreiranja; }
+            set { datumKreiranja = value; }
+        }
+
+        public TipRacuna Tip
+        {
+            get { return tip; }
+            set { tip = value; }
+        }
+
+        public String BrojRacuna
+        {
+            get { return brojRacuna; }
+            set { brojRacuna = value; }
+        }
+
+        public long ID
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string RetrivePKFieldName()
+        {
+            return Konstante.Konstante.
+            //return String.Concat(Konstante.T.PK_CLIENT_ID_FIELD, Konstante., Konstante.AccountTable.PK_ACCOUNT_ID_FIELD);
+        }
+
+        public string RetriveTableName()
+        {
+          //  return Constants.AccountTable.TABLE_NAME;
+        }
+
+        public string RetriveValuesForInsertation()
+        {
+          //  return String.Format(Constants.AccountTable.ACCOUNT_TABLE_INSERT, this.id, this.brojRacuna, this.tip, this.datumKreiranja.ToString(Constants.SQL.DATE_FORMAT));
+        }
+    }
+
+    public enum TipRacuna
+    {
+        RSD,
+        EUR,
+        USD
+    }
+}
+}

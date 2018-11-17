0911db205417e9147022b06c907fd5ffab87a906
Nikola Manojlović <manojlovic.nikola95@gmail.com>
2018-11-17 16:59:07 +0100

    Nekoriscene biblioteke obrisane

--- Domen/Konstante.cs
+++ Domen/Konstante.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Domen
+﻿namespace Domen
 {
     public static class Konstante
     {
